61339084c2042930d8843370a539e0a3cc5b382b
kelsi <kelsi@10.151.87.189>
2019-02-07 00:32:45 -0800

    First Commit

---
+++ Term2/src/Term2.java
@@ -0,0 +1,14 @@
+
+public class Term2 {
+
+	public static void main(String[] args) {
+    	
+	    int num1 = 2;
+	    int num2 = 6;
+	        
+	    // Calculating product of two numbers
+	    int product = num1*num2;
+	    System.out.println("Output: "+product);
+	}
+	
+}
